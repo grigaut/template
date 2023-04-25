06c0aecb3e2a3175810db2224a719d7fe9a4c64a
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-04-25 14:29:49 +0200

    cookiecutter template initial push

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,14 @@
+import re
+import sys
+from cookiecutter.main import cookiecutter
+
+
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+package_name = "{{ cookiecutter.project_name|lower|replace('-', '_') }}"
+
+if not re.match(MODULE_REGEX, package_name):
+    print('ERROR: %s is not a valid Python module name!' % package_name)
+
+    # exits with status 1 to indicate failure
+    sys.exit(1)
