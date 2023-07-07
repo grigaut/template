f521a77c5a6cce99485e7119f4dd9db059b765f7
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-07-07 11:18:48 +0200

    New version

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,7 +5,7 @@ from cookiecutter.main import cookiecutter
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-package_name = "{{ cookiecutter.project_name|lower|replace('-', '_') }}"
+package_name = "{{ cookiecutter.package_name|lower|replace('-', '_') }}"
 
 if not re.match(MODULE_REGEX, package_name):
     print('ERROR: %s is not a valid Python module name!' % package_name)
--- {{cookiecutter.package_name}}/docs/scripts/pages_generator.py
+++ {{cookiecutter.package_name}}/docs/scripts/pages_generator.py
@@ -6,9 +6,9 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src/{{cookiecutter.project_name|lower|replace('-', '_')}}").rglob("*.py")):
+for path in sorted(Path("src/{{cookiecutter.package_name|lower|replace('-', '_')}}").rglob("*.py")):
     module_path = path.relative_to(".").with_suffix("")
-    doc_path = path.relative_to("src/{{cookiecutter.project_name|lower|replace('-', '_')}}").with_suffix(".md")
+    doc_path = path.relative_to("src/{{cookiecutter.package_name|lower|replace('-', '_')}}").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
